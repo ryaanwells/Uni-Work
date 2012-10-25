1626338e19b3ac7ac3889528ff9237fe18b04cef
Ryan Wells <1002253w@student.gla.ac.uk>
2012-10-25 12:24:56 +0100

    file transfer to laptop for work

--- AP3/Assessed1/mlist.c
+++ AP3/Assessed1/mlist.c
@@ -2,54 +2,70 @@
 #define _MLIST_H_
 
 #include "mentry.h"
+#include "stdlib.h"
 #define  STARTSIZE 10000
 
 typedef struct mlist{
 	unsigned int size;
-	struct MListHead *buckets;
+	struct mlisthead *buckets;
 } MList;
 
 typedef struct mlisthead{
-	struct MListNode *node;
+	struct mlistnode *node;
 	int entryTotal;
 	int isFull;
 } MListHead;
 
 typedef struct mlistnode{
-  struct MEntry *ment;
-  struct MEntry *next;
+	struct mentry *ment;
+	struct mlistnode *next;
 } MListNode;
 
-extern int ml_verbose;		/* if true, prints diagnostics on stderr */
+int ml_verbose = 0;/* if true, prints diagnostics on stderr */
 
 
 /* ml_lookup - looks for MEntry in the list, returns matching entry or NULL */
 MEntry *ml_lookup(MList *ml, MEntry *me){
-  MListHead *bkts = ml->buckets;
-  unsigned int total = ml->size;
-  MListNode *ptr;
-  MEntry *other;
-  unsigned int i = 0;
-  for (i;i<total;i++){
-    ptr = bkts[i].node;
-    while (ptr!= NULL){
-      if (me_compare(me, ptr->ment) == 0){
-	return (ptr->ment);
-      }
-      ptr = ptr->next;
-    }
-  }
-  return NULL;
+	MListHead *bkts =(MListHead *) ml->buckets;
+	unsigned int total = ml->size;
+	MListNode *ptr;
+	MListHead *hptr;
+	MEntry *other;
+	unsigned int i = 0;
+	for (i;i<total;i++){
+	
+		
+		ptr =(MListNode *) bkts[i].node;
+		while (ptr!= NULL){
+			if (me_compare(me,(MEntry *) ptr->ment) == 0){
+				return ((MEntry *)ptr->ment);
+			}
+			ptr =(MListNode *) ptr->next;
+		}
+	}
+	return NULL;
 }
 
-MList *ml_create_size(unsigned int size){
-	MList *ml = (MList *) malloc(sizeof(MList)); 
+static MList *ml_create_size(unsigned int size){
+	MList *ml =(MList *) malloc(sizeof(MList)); 
 	if (ml == NULL) return NULL;
-	MListHead *bkts = malloc(sizeof(MListHead)*size);
+	MListHead *bkts =(MListHead *) malloc(sizeof(MListHead)*size);
 	if (bkts = NULL) return NULL;
+	int i = 0;
+	MListHead *ptr = bkts;
+	MListHead *mlnew;
+	while(i<size){
+		mlnew = malloc(sizeof(MListHead));
+		mlnew->node = NULL;
+		mlnew->entryTotal = 0;
+		mlnew->isFull = 0;
+		*ptr = *mlnew;
+		i++;
+		ptr++;
+	}
 	ml->size = size;
-	ml->buckets = &bkts;
-	return &ml;
+	ml->buckets = bkts;
+	return ml;
 }
 
 /* ml_create - created a new mailing list */
@@ -63,19 +79,20 @@ MList *ml_create(void){
  * returns 1 if successful, 0 if error (malloc)
  * returns 1 if it is a duplicate */
 int ml_add(MList **ml, MEntry *me){
-  MList *mlpointer = *ml;
-  MEntry *MEpoint = ml_lookup(mlpointer,me);
+	MList *mlpointer = *ml;
+	MEntry *MEpoint = ml_lookup(mlpointer,me);
 	if (MEpoint != NULL){ return 1;}
 	unsigned int mlsize = mlpointer->size;
 	unsigned long hval;
 	hval = (me,mlsize);
-	MListHead *buckets = &mlpointer->buckets;
+	MListHead *buckets =(MListHead *) mlpointer->buckets;
 	MListHead *location = &buckets[hval];
 	MListNode *newnode = malloc(sizeof(MListNode));
 	if (newnode == NULL){ return 0;}
 	newnode->ment = me;
+	newnode->next = NULL;
 	if (location->node == NULL){
-		location->node = &newnode;
+		location->node = newnode;
 		location->entryTotal = 1;
 		location->isFull = 0;
 		return 1;
@@ -84,7 +101,7 @@ int ml_add(MList **ml, MEntry *me){
 	while (npoint->next != NULL){
 		npoint = npoint->next;
 	}
-	npoint->next = me;
+	npoint->next = newnode;
 	location->entryTotal++;
 	return 1;
 }
@@ -93,7 +110,7 @@ int ml_add(MList **ml, MEntry *me){
 
 /* ml_destroy - destroy the mailing list */
 void ml_destroy(MList *ml){
-  return;
+	return;
 }
 
 #endif /* _MLIST_H_ */
