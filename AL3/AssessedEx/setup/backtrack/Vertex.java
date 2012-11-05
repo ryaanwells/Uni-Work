b4219ba1c1305c341a582561cedbccedce794755
Ryan Wells <ryaanwells@googlemail.com>
2012-11-05 22:50:46 +0000

    organised backtrack/

---
+++ AL3/AssessedEx/setup/backtrack/Vertex.java
@@ -0,0 +1,93 @@
+import java.util.LinkedList;
+
+
+/**
+ class to represent a vertex in a graph
+*/
+public class Vertex {
+   
+    private LinkedList<AdjListNode> adjList ; // the adjacency list of the vertex 
+    private int index; // the index of the vertex
+    private int distance;
+    
+    // possibly other fields, for example representing data
+    // stored at the node, whether the vertex has been visited
+    // in a traversal, its predecessor in such a traversal, etc.
+
+    boolean visited; // whether vertex has been visited in a traversal
+    int predecessor = -1; // index of predecessor vertex in a traversal
+
+    /**
+	 creates a new instance of Vertex
+	*/
+    public Vertex(int n) {
+    	adjList = new LinkedList<AdjListNode>();
+    	index = n;
+    	visited = false;
+    	distance = -1;
+    }
+    
+    /**
+	 copy constructor
+	*/
+    public Vertex(Vertex v){
+    	adjList = v.getAdjList();
+    	index = v.getIndex();
+    	visited = v.getVisited();
+    	distance = v.getDistance();
+    	predecessor = v.getPredecessor();
+    }
+     
+    public LinkedList<AdjListNode> getAdjList(){
+        return adjList;
+    }
+    
+    public int getIndex(){
+    	return index;
+    }
+    
+    public void setIndex(int n){
+    	index = n;
+    }
+    
+    public boolean getVisited(){
+    	return visited;
+    }
+    
+    public void setVisited(boolean b){
+    	visited = b;
+    }
+    
+    public int getPredecessor(){
+    	return predecessor;
+    }
+    
+    public void setPredecessor(int n){
+    	predecessor = n;
+    }
+    
+    public int getDistance(){
+    	return distance;
+    }
+    
+    public void setDistance(int d){
+    	distance = d;
+    }
+    
+    public void addToAdjList(int n, int w){
+        adjList.addLast(new AdjListNode(n,w));
+    }
+    
+    public int vertexDegree(){
+        return adjList.size();
+    }
+    
+	public int wt(Vertex x){
+		for(AdjListNode A: this.adjList){
+			if(A.getVertexNumber() == x.getIndex()){
+				return A.getWeight();
+			}
+		}
+		return -1;
+	}
+}
--- AL3/AssessedEx/setup/dijkstra/Main.java
+++ AL3/AssessedEx/setup/dijkstra/Main.java
@@ -5,138 +5,132 @@ import java.lang.Math;
 
 /**
  program to find shortest path using Dijkstra's algorithm
- */
+*/
 public class Main {
 
-	public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException {
 
-		long start = System.currentTimeMillis();
+	long start = System.currentTimeMillis();
 
-		String inputFileName = args[0]; // input file name
+	String inputFileName = args[0]; // input file name
   
-		FileReader reader = new FileReader(inputFileName);
-		Scanner in = new Scanner(reader);
-		
-		int floatNode, sinkNode, vWeight;
-		int nodeTotal = in.nextInt();
-		
-		Graph dGraph = new Graph(nodeTotal);
-		for (int i = 0; i<nodeTotal; i++){
-			for (int j = 0; j<nodeTotal; j++){
-				vWeight = in.nextInt();
-				if (vWeight != 0){
-					dGraph.getVertex(i).addToAdjList(j,vWeight);
-				}
-			}
-		}
-		floatNode = in.nextInt();
-		sinkNode = in.nextInt();
+	FileReader reader = new FileReader(inputFileName);
+	Scanner in = new Scanner(reader);
 
-		reader.close();
-		
-		int[] S = new int[nodeTotal];
-		int[] d = new int[nodeTotal];
-		for(int i=0;i<nodeTotal;i++){
-			S[i]=0;
-			d[i]=dGraph.getVertex(floatNode).wt(dGraph.getVertex(i));
-			if (d[i]!=-1){
-				dGraph.getVertex(i).setPredecessor(floatNode);
-			}
-			else{
-				dGraph.getVertex(i).setPredecessor(-1);
-			}
+	int floatNode, sinkNode, vWeight;
+	int nodeTotal = Integer.parseInt(in.next());
+
+	Graph dGraph = new Graph(nodeTotal);
+	for (int i = 0; i<nodeTotal; i++){
+	    for (int j = 0; j<nodeTotal; j++){
+		vWeight = Integer.parseInt(in.next());
+		if (vWeight != 0){
+		    dGraph.getVertex(i).addToAdjList(j,vWeight);
 		}
-		
-		S[floatNode]=1;
-		boolean active = false;
-		boolean found = false;
-		
-		
-		for(int i=0; i<nodeTotal;i++){
-			if(S[i]!=1 && d[i]>=0){
-				active = true;
-			}
-		}
-		
-		
-		int leastDistance = Integer.MAX_VALUE;
-		int newestNode = 0;
-		
-		
-		while (active){
-			leastDistance = Integer.MAX_VALUE;
-			newestNode = -1;
-			for(int i=0; i<nodeTotal;i++){
-				if (S[i]!=1 && (d[i]>0 && d[i]<=leastDistance)){
-					leastDistance = d[i];
-					newestNode = i;
-				}
-			}
-			if(newestNode == -1){
-				active = false;
-				break;
-			}
-			S[newestNode]=1;
-			if(newestNode == sinkNode){
-				active = false;
-				found = true;
-				continue;
-			}
-			for(int i=0; i<nodeTotal; i++){
-				if(S[i]!=1){
-					if(d[i] == -1){
-						d[i] = dGraph.getVertex(newestNode).wt(dGraph.getVertex(i));
-						if (d[i] != -1){
-							dGraph.getVertex(i).setPredecessor(newestNode);
-							d[i] = dGraph.getVertex(newestNode).wt(dGraph.getVertex(i)) +
-								d[newestNode];
-						}
-					}
-					else{
-						int distance = dGraph.getVertex(newestNode).wt(dGraph.getVertex(i));
-						if(distance != -1){
-							if(d[i] > d[newestNode]+distance){
-								d[i] = d[newestNode]+distance;
-								dGraph.getVertex(i).setPredecessor(newestNode);
-							}
-						}
-					}
-				}
-			}
-			
-			active = false;
-			if(!found){
-				for(int i=0; i<nodeTotal;i++){
-					if(S[i]!=1 && d[i]!=-1){
-						active = true;
-					}
-				}
-			}
-		}
-		
-		if(!found){
-			System.out.println("\nNo route to the sink node could be found.");
-		}
-		else{
-			System.out.println("Shortest distance from vertex " + floatNode + " to vertex " + sinkNode + " is " + d[sinkNode]);
-			Stack<Integer> reverse = new Stack<Integer>();
-			int parent = sinkNode;
-			while (parent!=floatNode){
-				reverse.add(parent);
-				parent = dGraph.getVertex(parent).getPredecessor();
-			}
-			reverse.add(floatNode);
-			System.out.print("Shortest path: ");
-			while (!reverse.isEmpty()){
-				System.out.print(reverse.pop());
-				System.out.print(" ");
-			}
-		}
-		
-		// end timer and print total time
-		long end = System.currentTimeMillis();
-		System.out.println("\nElapsed time: " + (end - start) + " milliseconds");
+	    }
 	}
-	
+	floatNode = Integer.parseInt(in.next());
+	sinkNode = Integer.parseInt(in.next());
+
+	reader.close();
+
+	int[] S = new int[nodeTotal];
+	int[] d = new int[nodeTotal];
+	for(int i=0;i<nodeTotal;i++){
+	    S[i]=0;
+	    d[i]=dGraph.getVertex(floatNode).wt(dGraph.getVertex(i));
+	    if (d[i]!=-1){
+		dGraph.getVertex(i).setPredecessor(floatNode);
+	    }
+	    else{
+		dGraph.getVertex(i).setPredecessor(-1);
+	    }
+	}
+
+	S[floatNode]=1;
+	boolean active = false;
+	boolean found = false;
+
+
+	for(int i=0; i<nodeTotal;i++){
+	    if(S[i]!=1 && d[i]>=0){
+		active = true;
+	    }
+	}
+
+	while (active){
+	    int leastDistance = Integer.MAX_VALUE;
+	    int newestNode = -1;
+	    for(int i=0; i<nodeTotal;i++){
+		if (S[i]!=1 && (d[i]>0 && d[i]<=leastDistance)){
+		    leastDistance = d[i];
+		    newestNode = i;
+		}
+	    }
+	    if(newestNode == -1){
+		active = false;
+		break;
+	    }
+	    S[newestNode]=1;
+	    if(newestNode == sinkNode){
+		active = false;
+		found = true;
+		break;
+	    }
+	    for(int i=0; i<nodeTotal; i++){
+		if(S[i]!=1){
+		    if(d[i] == -1){
+			d[i] = dGraph.getVertex(newestNode).wt(dGraph.getVertex(i));
+			if (d[i] != -1){
+			    dGraph.getVertex(i).setPredecessor(newestNode);
+			    d[i] = dGraph.getVertex(newestNode).wt(dGraph.getVertex(i)) +d[newestNode];
+			}
+		    }
+		    else{
+			int distance = dGraph.getVertex(newestNode).wt(dGraph.getVertex(i));
+			if(distance != -1){
+			    if(d[i] > d[newestNode]+distance){
+				d[i] = d[newestNode]+distance;
+				dGraph.getVertex(i).setPredecessor(newestNode);
+			    }
+			}
+		    }
+		}
+	    }
+
+	    active = false;
+	    if(!found){
+		for(int i=0; i<nodeTotal;i++){
+		    if(S[i]!=1 && d[i]!=-1){
+			active = true;
+		    }
+		}
+	    }
+	}
+	long end = System.currentTimeMillis();
+	if(!found){
+	    System.out.println("\nNo route to the sink node could be found.");
+	}
+	else{
+	    System.out.println("Shortest distance from vertex " + floatNode + " to vertex " + sinkNode + " is " + d[sinkNode]);
+	    Stack<Integer> reverse = new Stack<Integer>();
+	    int parent = sinkNode;
+	    while (parent!=floatNode){
+		reverse.add(parent);
+		parent = dGraph.getVertex(parent).getPredecessor();
+	    }
+	    reverse.add(floatNode);
+	    System.out.print("Shortest path: ");
+	    while (!reverse.isEmpty()){
+		System.out.print(reverse.pop());
+		System.out.print(" ");
+	    }
+	}
+
+	// end timer and print total time
+
+	System.out.println("\nElapsed time: " + (end - start) + " milliseconds");
+    }
+
 
 }
