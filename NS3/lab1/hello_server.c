0a542a7e717ca0c3473ed53bf798823990f40a33
ryaanwells <ryaanwells@googlemail.com>
2013-01-09 15:56:34 +0000

    Started NS3

---
+++ NS3/lab1/hello_server.c
@@ -0,0 +1,61 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <stdio.h>
+
+#define BUFLEN 1500
+
+int main(int argc, char* argv[])
+{
+	int fd, connfd;
+	struct sockaddr_in addr, cliaddr;
+	socklen_t cliaddrlen = sizeof(cliaddr);
+	ssize_t i, rcount;
+	char buf[BUFLEN];
+
+	addr.sin_addr.s_addr = INADDR_ANY;
+	addr.sin_family = AF_INET;
+	addr.sin_port = htons(5000);
+	// Make Socket
+	if(fd = socket(AF_INET,SOCK_STREAM,0)==-1){
+		//deal with no socket
+		fprintf(stderr,"%s","Socket not connected");
+		return -1;
+	}
+	//Bind
+	if(bind(fd, (struct sockaddr *)&addr,sizeof(addr)) == -1){
+		//deal with no connection
+		fprintf(stderr,"%s","Bind not made");
+		close(fd);
+		return -1;
+	}
+	//Listen
+	if(listen(fd, 1) == -1){
+		//deal with failure
+		fprintf(stderr,"%s","Listen failed");
+		close (fd);
+		return -1;
+	}
+	//Connect
+	if((connfd = accept(fd, (struct sockaddr *) &cliaddr, &cliaddrlen)) ==-1){
+		//deal with failure
+		fprintf(stderr,"%s","accept failed");
+		close (fd);
+		return -1;
+	}
+	//Read
+	if((rcount=read(connfd,buf,BUFLEN))==-1){
+		//deal with failure
+		fprintf(stderr,"%s","read failed");
+	}
+	for(i=0; i < rcount; i++){
+		if(buf[i] == EOF){
+			close(connfd);
+		}
+		printf("%c",buf[i]);
+	}
+	//Close
+}
+
