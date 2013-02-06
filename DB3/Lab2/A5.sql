5f4239e26e56729c8ec005fab36246d42eb9e093
ryaanwells <ryaanwells@googlemail.com>
2013-02-06 22:14:39 +0000

    DB3 DONE

--- DB3/Lab2/A5.sql
+++ DB3/Lab2/A5.sql
@@ -1,24 +1,41 @@
-SELECT BAND.NAME AS Band_Name, MEMBER.NAME AS Singer_Name, RELEASE.TITLE AS Album_Name, MostProlific.Total AS Song_Num
-From BAND, RELEASE, MEMBEROF, MEMBER, (
+CREATE INDEX meminst ON MemberOf(Instrument)
+go
+SELECT BAND.NAME AS Band_Name, MEMBER.NAME AS Singer_Name,
+RELEASE.TITLE AS Album_Name, MostProlific.Total AS Song_Num
+From BAND, RELEASE, MEMBEROF, MEMBER, 
+(
     SELECT AlbumTracks.RID, AlbumTracks.Total
-    FROM (
+    FROM 
+    (
         SELECT RELEASE.RID, COUNT(RELEASE.Title) AS total
         FROM RELEASE, SONG
         WHERE RELEASE.RID=SONG.RID 
         GROUP BY RELEASE.RID
         ORDER BY COUNT(RELEASE.Title) DESC
         ) AlbumTracks 
-    INNER JOIN (
+        INNER JOIN 
+        (
         SELECT MAX(COUNT(RELEASE.Title)) AS total
         FROM RELEASE, SONG, MEMBEROF
         WHERE RELEASE.RID=SONG.RID AND RELEASE.BID=MEMBEROF.BID 
-        AND (RELEASE.YEAR>=MEMBEROF.STARTYEAR OR MEMBEROF.STARTYEAR IS NULL)
-        AND (RELEASE.YEAR<=MEMBEROF.ENDYEAR OR MEMBEROF.ENDYEAR IS NULL) 
+        AND 
+        (   
+             RELEASE.YEAR>=MEMBEROF.STARTYEAR 
+             OR 
+             MEMBEROF.STARTYEAR IS NULL
+        )
+        AND 
+        (
+             RELEASE.YEAR<=MEMBEROF.ENDYEAR 
+             OR 
+             MEMBEROF.ENDYEAR IS NULL
+        ) 
         AND MEMBEROF.INSTRUMENT='vocals'
         GROUP BY RELEASE.RID
         ORDER BY COUNT(RELEASE.TITLE) DESC
-        ) MostSongs
-    ON AlbumTracks.total=MostSongs.total) MostProlific
+    ) MostSongs
+    ON AlbumTracks.total=MostSongs.total
+) MostProlific
 WHERE BAND.BID=Release.BID AND Release.RID=MostProlific.RID 
 AND MEMBEROF.INSTRUMENT='vocals' AND MEMBEROF.MID=MEMBER.MID
 AND (MEMBEROF.STARTYEAR <= Release.YEAR or MEMBEROF.STARTYEAR IS NULL)
