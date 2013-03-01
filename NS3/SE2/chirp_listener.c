3460cb9ea9716b22ca8977d287e1313c2f4d6bec
ryaanwells <ryaanwells@googlemail.com>
2013-03-01 16:05:53 +0000

    NS3 not parsing correctly

--- NS3/SE2/chirp_listener.c
+++ NS3/SE2/chirp_listener.c
@@ -6,18 +6,33 @@
 #include <stdio.h>
 #include <errno.h>
 #include <string.h>
+#include <ctype.h>
+#include <time.h>
 
-#define BUF_LEN 1000
+#define BUF_LEN 1001
+
+void clean(char * string){
+	int charTot = strlen(string);
+	int i;
+	for(i=0;i<charTot;i++){
+		if(!isprint(string[i]))string[i]='?';
+	}
+}
 
 int main(int argc, char* argv[]){
 	
 	int fd;
 	char buffer[BUF_LEN];
+	char fullmsg[BUF_LEN+50];
+	char * from, * msg;
+	char dformat[] = "%c - %%s - %%s";
 	struct sockaddr addr;
 	struct sockaddr_in servaddr;
 	struct ip_mreq imr;
 	socklen_t alen=sizeof(addr);
 	int rlen;
+	time_t ct;
+	struct tm *timeinfo;
 	memset(&servaddr,0,sizeof(servaddr));
 	if(argc!=1){
 		fprintf(stderr,"Usage './chirp_listener'\n");
@@ -48,14 +63,33 @@ int main(int argc, char* argv[]){
 		return -1;
 	}
 	while(1){
+		from = msg = NULL;
 		rlen = recvfrom(fd,buffer,BUF_LEN,0,(struct sockaddr *)&addr,&alen);
 		if (rlen<0){
 			fprintf(stderr,"RLEN<0\n");
 			continue;
 		}
 		buffer[rlen] = '\0';
-		fprintf(stdout,"Message:\n%-*.*s\n",rlen,rlen,buffer);
+		time(&ct);
+		timeinfo = localtime(&ct);
+		strftime(fullmsg,BUF_LEN+50,dformat,timeinfo);
+		from = strstr(buffer,"FROM");
+		msg = strstr(buffer,"\n");
+		if(from!=NULL&&msg!=NULL){
+			printf("first Inner\n");
+			from = from+5;
+			if((msg-from)<=16){
+				*(msg++)='\0';
+				clean(from);
+				clean(msg+1);
+				buffer[rlen]='\0';
+				printf("second Inner\n");
+				sprintf(fullmsg,fullmsg,from,msg);
+				fprintf(stdout,"%-*.*s\n",rlen+50,rlen+50,fullmsg);
+			}
+		}
 		buffer[0]='\0';
+		printf("here\n");
 	}
 	return 1;
 }
