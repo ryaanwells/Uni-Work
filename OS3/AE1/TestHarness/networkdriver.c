52421a4d8afa44a02ed2a28245f1de4fcfec75a8
ryaanwells <ryaanwells@googlemail.com>
2013-03-03 11:11:47 +0000

    fixed OS3 AE1

--- OS3/AE1/TestHarness/networkdriver.c
+++ OS3/AE1/TestHarness/networkdriver.c
@@ -60,13 +60,6 @@ int  nonblocking_send_packet(PacketDescriptor PD){
   DIAGNOSTICS("Attempting to add non blocking for PID:%d\n",packet_descriptor_get_pid(&PD));
   return nonblockingWriteBB(sendBuffer,PD);
 }
-/* These calls hand in a PacketDescriptor for dispatching */
-/* The nonblocking call must return promptly, indicating whether or */
-/* not the indicated packet has been accepted by your code          */
-/* (it might not be if your internal buffer is full) 1=OK, 0=not OK */
-/* The blocking call will usually return promptly, but there may be */
-/* a delay while it waits for space in your buffers.                */
-/* Neither call should delay until the packet is actually sent      */
 
 void blocking_get_packet(PacketDescriptor* PD, PID pid){
   DIAGNOSTICS("Attempting to get blocking for PID:%d\n",pid);
@@ -74,22 +67,10 @@ void blocking_get_packet(PacketDescriptor* PD, PID pid){
 }
 
 int  nonblocking_get_packet(PacketDescriptor* PD, PID pid){
-  DIAGNOSTICS("Attempting to get blocking for PID:%d\n",pid);
+  DIAGNOSTICS("Attempting to get non blocking for PID:%d\n",pid);
   *PD = getFromTSA(recvArray,pid);
   return(PD == NULL? 0:1);
 }
-/* These represent requests for packets by the application threads */
-/* The nonblocking call must return promptly, with the result 1 if */
-/* a packet was found (and the first argument set accordingly) or  */
-/* 0 if no packet was waiting.                                     */
-/* The blocking call only returns when a packet has been received  */
-/* for the indicated process, and the first arg points at it.      */
-/* Both calls indicate their process number and should only be     */
-/* given appropriate packets. You may use a small bounded buffer   */
-/* to hold packets that haven't yet been collected by a process,   */
-/* but are also allowed to discard extra packets if at least one   */
-/* is waiting uncollected for the same PID. i.e. applications must */
-/* collect their packets reasonably promptly, or risk packet loss. */
 
 void init_network_driver(NetworkDevice               nd, 
                          void *                      mem_start, 
@@ -98,7 +79,7 @@ void init_network_driver(NetworkDevice               nd,
   DIAGNOSTICS("INIT: Creating FPDS\n");
   FPDS = create_fpds();
   create_free_packet_descriptors(FPDS,mem_start,mem_length);
-  fpds_ptr = FPDS;
+  *fpds_ptr = FPDS;
   DIAGNOSTICS("INIT: Creating Buffers\n");
   sendBuffer = createBB(6);
   recvArray = createTSArray(MAX_PID+1);
@@ -109,12 +90,3 @@ void init_network_driver(NetworkDevice               nd,
   pthread_create(&readThread1,NULL,recvFromSocket,NULL);
   DIAGNOSTICS("INIT: Init complete.\n");
 }
-/* Called before any other methods, to allow you to initialise */
-/* data structures and start any internal threads.             */ 
-/* Arguments:                                                  */
-/*   nd: the NetworkDevice that you must drive,                */
-/*   mem_start, mem_length: some memory for PacketDescriptors  */
-/*   fpds_ptr: You hand back a FreePacketDescriptorStore into  */
-/*             which you have put the divided up memory        */
-/* Hint: just divide the memory up into pieces of the right size */
-/*       passing in pointers to each of them                     */ 
