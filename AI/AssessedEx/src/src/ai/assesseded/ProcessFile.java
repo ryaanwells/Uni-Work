5735ddf5f7cb104e4f4f9a2cfb0818e32aa169a5
Ryan Wells <ryaanwells@googlemail.com>
2013-10-17 22:49:52 +0100

    Got the first graph right

--- AI/AssessedEx/src/src/ai/assesseded/ProcessFile.java
+++ AI/AssessedEx/src/src/ai/assesseded/ProcessFile.java
@@ -1,6 +1,9 @@
 package src.ai.assesseded;
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
@@ -33,18 +36,41 @@ public class ProcessFile {
 		}
 	}
 	
-	public int getValueCount(){
+	public int getSampleCount(){
 		return values.size();
 	}
 	
 	public double getSamplingRate(){
-		return getValueCount()/this.sampleLength;
+		return getSampleCount()/this.sampleLength;
+	}
+	
+	public double getSamplesInWindow(double windowLength){
+		return getSampleCount() * (windowLength / this.sampleLength);
+	}
+	
+	public double[] getEnergy(int windowSamples){
+		int k, n; 
+		double energyValue = 0.0;
+		double[] E = new double[this.getSampleCount()];
+		for (n = 0; n < this.getSampleCount(); n++){
+			for (k = n; k >= n - windowSamples && k >= 0; k--){
+				energyValue += values.get(k) * values.get(k);
+			}
+			System.out.print(n + " ");
+			System.out.println(energyValue * 1000);
+						
+			E[n] = energyValue;
+			energyValue = 0.0;
+		}
+		System.out.println("windowSamples: " + windowSamples);
+		return E;
 	}
 	
 	public static void main(String args[]){
 		String filename = System.getProperty("user.dir") + "/laboratory.dat";
 		final double scaleFactor = 0.001;
 		final double sampleLength = 0.3;
+		final double windowSize = 0.03;
 		ProcessFile pf = new ProcessFile(filename, scaleFactor, sampleLength);
 		try {
 			pf.readFile();
@@ -52,8 +78,29 @@ public class ProcessFile {
 		catch (IOException e){
 			System.err.println("Could not read file");
 		}
-		System.out.println("Finished");
-		System.out.println(pf.getSamplingRate());
+		System.out.println("Finished Reading");
+		
+		Path energyData = Paths.get("energyData.txt");
+		try {
+			Files.deleteIfExists(energyData);
+			energyData = Files.createFile(energyData);
+		} catch(IOException e){
+			System.err.println("Could not create energyData.txt");
+		}
+		
+		try (BufferedWriter writer = Files.newBufferedWriter(energyData, Charset.defaultCharset())){
+			double[] energy = pf.getEnergy((int)pf.getSamplesInWindow(windowSize));
+			for (int i = 0; i < energy.length; i++){
+				writer.write(String.format("%.2f", energy[i]));
+				writer.newLine();
+			}
+			writer.flush();
+		} catch(IOException e){
+			System.err.println("Could not create buffered writer for energyData.txt");
+		}
+		
+		System.out.println(pf.getSamplesInWindow(windowSize));
+		
 	}
-
+	
 }
