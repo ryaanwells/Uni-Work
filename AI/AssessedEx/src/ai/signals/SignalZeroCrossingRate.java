67762efed73a8f9cc047f90d370da128e9648289
ryaanwells <ryaanwells@googlemail.com>
2013-11-28 09:49:10 +0000

    Fixed ZCR

--- AI/AssessedEx/src/ai/signals/SignalZeroCrossingRate.java
+++ AI/AssessedEx/src/ai/signals/SignalZeroCrossingRate.java
@@ -12,11 +12,11 @@ public class SignalZeroCrossingRate extends SignalProcess{
 		for (n = 0; n < Z.length; n++){
 			sign = 0.0;
 			for (k = n; k >= n - samplesInWindow && k >= 0; k--){
-				signCurrent = base[k] >= 0 ? 1 : -1;
-				signPrevious = (k-1) >= 0 ? (base[k-1] >= 0 ? 1 : -1) : 0;
+				signCurrent = (base[k] >= 0 ? 1 : -1);
+				signPrevious = ((k-1) >= 0 ? (base[k-1] >= 0 ? 1 : -1) : 0);
 				sign += Math.abs(signCurrent - signPrevious);
 			}
-			Z[n] = sign / 2 * samplesInWindow;
+			Z[n] = sign / (2 * samplesInWindow);
 		}
 		return new Signal(Z, signal.sampleLength());
 	}
--- AI/AssessedEx/src/src/ai/assesseded/Lab2.java
+++ AI/AssessedEx/src/src/ai/assesseded/Lab2.java
@@ -1,6 +1,9 @@
 package src.ai.assesseded;
 
+import java.io.BufferedWriter;
 import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Scanner;
@@ -31,8 +34,26 @@ public class Lab2 {
 		Signal magnitudeSignal;
 		Signal zeroCrossingSignal;
 		
+		Path writePath = Paths.get("speechValues.txt");
+		BufferedWriter out = null;;
+		
+		try {
+			Files.deleteIfExists(writePath);
+			writePath = Files.createFile(writePath);
+		} catch (IOException IOE){
+			System.err.println("Could not instantiate file.");
+			System.exit(0);
+		}
+		
+		try {
+			out = Files.newBufferedWriter(writePath, Charset.defaultCharset());
+		} catch (IOException IOE){
+			System.err.println("Could not create writer for file.");
+			System.exit(0);
+		}
+		
 		for (int i = 1; i < 51; i++){
-			path = Paths.get("audio/silence_" + (i > 9 ? i : "0" + i) + ".dat");
+			path = Paths.get("audio/speech_" + (i > 9 ? i : "0" + i) + ".dat");
 			try {
 				scanner = new Scanner(path);
 			} catch (IOException e) {
@@ -49,11 +70,20 @@ public class Lab2 {
 			energySignal = SE.process(signal, windowSize);
 			magnitudeSignal = SM.process(signal, windowSize);
 			zeroCrossingSignal = SZ.process(signal, windowSize);
-			System.out.println(energySignal.getAverage(false) + " " +
-					magnitudeSignal.getAverage(false) + " " +
+			
+			System.out.format("%.2f %.2f %.2f \n", Math.log(energySignal.getAverage(false)),
+					Math.log(magnitudeSignal.getAverage(false)),
 					zeroCrossingSignal.getAverage(false));
+			
+			try {
+				out.write(String.format("%.2f %.2f %.2f \n", Math.log(energySignal.getAverage(false)),
+						Math.log(magnitudeSignal.getAverage(false)),
+						zeroCrossingSignal.getAverage(false)));
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 		}
-
+		
 	}
 
 }
